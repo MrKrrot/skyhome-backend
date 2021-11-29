48d00133cbf2c4f807886e63369efb7564bc2135
MrKrrot <rafita.olguin@hotmail.com>
2021-11-29 15:49:06 -0600

    Changed decodedToken interface path to folder 'types'

--- src/middlewares/userExtractor.ts
+++ src/middlewares/userExtractor.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
-import { decodedToken } from '../interfaces/decodedToken'
+import { decodedToken } from '../../types/decodedToken'
 
 export default (req: Request, res: Response, next: NextFunction) => {
     const authorization = req.get('authorization')
